9c6d5aeab093eb55d9f99b9fc2dff2d5150d85ac
Olena Nesteruk <pavlena81@gmail.com>
2022-08-15 19:54:33 +0300

    adds convertMs

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,7 +10,10 @@ import flatpickr from "flatpickr";
 // Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
 
-flatpickr(document.getElementById('datetime-picker'), options);
+import"flatpickr/dist/themes/material_green.css";
+
+flatpickr(inputEl, options);
+const inputEl = document.getElementById('datetime-picker');
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -20,3 +23,33 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
+
+const ref = {
+    startBtn: document.querySelector('[data - start]'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds:document.querySelector('[data-seconds]'),
+}
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
