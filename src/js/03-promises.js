27e59cc1c36b8b6f327a3598e0dfef672fcee73b
Olena Nesteruk <pavlena81@gmail.com>
2022-08-18 00:33:39 +0300

    adds  correction task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,41 +17,51 @@
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const refs = {
-  formEl: document.querySelector('.form'),
-  delayEl: document.querySelector('input[name="delay"]'),
-  stepEl: document.querySelector('input[name="step"]'),
-  amountEl: document.querySelector('input[name="amount"]'),
-};
-
-refs.formEl.addEventListener('submit', createPromise);
+const formEl = document.querySelector('.form');
+  
+formEl.addEventListener('submit', onCreatePromise);
+// const makePromise = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(position), delay);
+//   });
+// }
+// makePromise()
+//   .than(result => console.log(result))
+//   .catch(error => console.log(error));
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+ return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } 
+        reject({ position, delay })
+      
+    }, delay)
+  })
+    .then(({ position, delay }) => {
+      return Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      return Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
 }
+function onCreatePromise(e) {
+  e.preventDefault();
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+   delay = Number(e.currentTarget.delay.value);
+   step = Number(e.currentTarget.step.value);
+   amount = Number(e.currentTarget.amount.value);
 
-//   const makePromise = (text, delay) => {
-//   return new Promise(resolve => {
-//     setTimeout(() => resolve(text), delay);
-//   });
-// };
-
-// const promiseA = makePromise("promiseA value", 1000);
-// const promiseB = makePromise("promiseB value", 3000);
-
-// Promise.all([promiseA, promiseB])
-//   .then(value => console.log(value)) //["promiseA value", "promiseB value"]
-//   .catch(error => console.log(error));
+  if (delay >= 0 && step >=0 && amount > 0) {
+    for (let position = 1; position <= amount; position += 1 ){
+    createPromise(position, delay);
+    delay += step;
+  }
+  } else {
+    return Notify.warning('input value >0');
+  }
+  
+ }
